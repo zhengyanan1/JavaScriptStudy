4fe71b94e3fbfbf509d45d1a2307a32fd9c5a575
hardenzheng <hardenzheng@tencent.com>
2022-04-11 19:24:41 +0800

    同步代码

---
+++ ch5/announcement.js
@@ -0,0 +1,25 @@
+
+
+swim()
+
+function swim(){
+    console.log("go swiming~")
+}
+
+// let man = new Harden("harden")
+
+class Harden{
+    constructor(name){
+        this.name = name
+    }
+}
+let man = new Harden("harden")
+
+
+console.log(("" || null )+ "***")
+
+// let obj = {
+//     a: 1
+// }
+// console.log(JSON.stringify(obj))
+// console.log(JSON.parse("{}"))
---
+++ ch5/strict.js
@@ -0,0 +1,10 @@
+
+
+let a = {
+    b: 1
+}
+console.log(a)
+delete a.c
+console.log(a)
+
+eval("console.log('hello')")
