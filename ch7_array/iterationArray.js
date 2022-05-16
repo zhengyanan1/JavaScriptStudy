63c4aadbad277058d39a2e40efb3314af6a11315
hardenzheng <2821084381@qq.com>
2022-05-16 08:18:10 +0800

    同步第7章

--- ch7_array/iterationArray.js
+++ ch7_array/iterationArray.js
@@ -11,3 +11,28 @@ a.forEach((num , index, nums)=> {
 
 let i = 0, b = 3
 console.log(i, b)
+
+
+a = [1, 2, [3, 4, [5]]]
+console.log(a)
+// console.log(a.flat(3))
+console.log(a.flatMap((value)=>{
+    return value
+}))
+
+
+
+a = []
+a.push(1, 4, 5)
+console.log(a)
+
+a = new Array(5)
+a.fill(2)
+console.log(a.lastIndexOf(2, 5))
+
+
+a = [1, 3, 2, 4]
+console.log(a.sort())
+console.log(a.sort((a, b)=> b - a))
+
+console.log(a.reverse())
