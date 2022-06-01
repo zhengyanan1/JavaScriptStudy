229d4b74389081b1a5d4d9e6155407f910c23649
hardenzheng <hardenzheng@tencent.com>
2022-06-01 19:44:06 +0800

    13章代码

---
+++ ch13_promise/start.js
@@ -0,0 +1,39 @@
+
+function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+async function GetNameFromNet(){
+    await sleep(1000)
+    // throw new Error("net error")
+    return "Harden"
+}
+
+
+// throw new Error("main error")
+
+// method1: catch 中捕获异常...
+GetNameFromNet().then(
+    (v)=>{
+        console.warn(v)
+        throw new Error("then error")
+    }).catch(
+    ((e)=>{
+        console.warn(e)
+    }))
+
+// GetNameFromNet().then(
+//     (v)=>{
+//         console.warn(v)
+//         throw new Error("then error")
+//     },
+//     (e)=>{
+//         console.warn(e)
+//     }
+// )
+
+async function test(){
+    await GetNameFromNet().then( (v)=>{console.warn("test ", v)})
+    console.warn("test")
+}
+console.warn("hello promise")
+test()
