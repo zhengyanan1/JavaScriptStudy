02da2c8d3048bae3106aa4854f12a6b803cf6b2c
hardenzheng <2821084381@qq.com>
2022-06-20 08:04:38 +0800

    update 15chapter

--- ch15_webJs/digital_clock.js
+++ ch15_webJs/digital_clock.js
@@ -2,7 +2,9 @@ function displayTIme(){
     let clock = document.querySelector("#clock")
     let now = new Date()
     clock.textContent = now.toLocaleTimeString()
+    // console.log(screen)
 }
 
 displayTIme()
 setInterval(displayTIme, 1000)
+console.log("readyState:", document.readyState)
