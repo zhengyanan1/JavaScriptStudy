1c7c25d7c21627305b3f5c494f4004d5259ee107
hardenzheng <hardenzheng@tencent.com>
2022-07-23 15:58:19 +0800

    feat: webpack5 开发环境配置

--- webpack5/WEBPACK_CODE/src/main.js
+++ webpack5/WEBPACK_CODE/src/main.js
@@ -1,5 +1,12 @@
 import count from "./js/count";
 import sum from "./js/sum";
 
-console.log(count(2, 1))
-console.log(sum(1, 2, 3, 4))
+import "./css/iconfont.css"
+import "./css/index.css"
+import "./less/index.less"
+import "./sass/index.sass"
+import "./sass/index.scss"
+import "./stylus/index.styl"
+
+console.log(count(1, 23))
+console.log(sum(1, 2, 3, 4, 5, 6))
