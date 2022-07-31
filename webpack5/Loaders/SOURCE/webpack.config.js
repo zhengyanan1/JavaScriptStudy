8df5fc6accc980e59a43c8cba085a86a29d8a43f
hardenzheng <hardenzheng@tencent.com>
2022-07-31 11:00:34 +0800

    feat: webpack5 之 AnalyzeWebpackPlugin

--- webpack5/Loaders/SOURCE/webpack.config.js
+++ webpack5/Loaders/SOURCE/webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 const TestPlugin = require("./plugins/test-plugin")
 const BannerWebpackPlugin = require("./plugins/banner-webpack-plugin")
 const CleanWebpackPlugin = require("./plugins/clean-webpack-plugin")
+const AnalyzeWebpackPlugin = require("./plugins/analyze-webpack-plugin")
 
 
 
@@ -68,7 +69,8 @@ module.exports = {
         new BannerWebpackPlugin({
             author: '老王'
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new AnalyzeWebpackPlugin()
     ],
     mode: 'production'
 }
