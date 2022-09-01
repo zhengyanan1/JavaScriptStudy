2b3eef5bfc6aa808ff765c80010c0b7a4cf02219
nobody <11536374+hardenzheng@user.noreply.gitee.com>
2022-09-02 00:12:05 +0800

    test: 测试代码

--- test/testJs.js
+++ test/testJs.js
@@ -8,3 +8,17 @@ let a = "012"
 a.charAt(0)
 
 console.log(path.join("./../../", 'harden'))
+
+console.log(NaN === NaN)
+let b
+console.log(b === undefined)
+let arr = []
+arr.unshift(1)
+arr.unshift(2)
+console.log(arr)
+
+arr.every((num, index)=>{
+    console.log(num)
+    return num > 0
+})
+
