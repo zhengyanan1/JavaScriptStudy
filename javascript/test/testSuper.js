fdab6930c28ba02bdc954ad31e6e0606351cf80d
hardenzheng <hardenzheng@tencent.com>
2022-09-09 17:56:37 +0800

    test: js测试super

---
+++ javascript/test/testSuper.js
@@ -0,0 +1,24 @@
+class sup {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    printName() {
+      console.log(this.name);
+    }
+  }
+  
+  class sub extends sup {
+    constructor(name, age) {
+      super(name); // super代表的事父类的构造函数
+      this.age = age;
+    }
+  
+    printAge() {
+      console.log(this.age);
+    }
+  }
+  
+  let jack = new sub("jack", 20);
+  jack.printName(); //输出 : jack
+  jack.printAge(); //输出 : 20
