636648f77f8e929031712713d091d7e5956f76db
hardenzheng <hardenzheng@tencent.com>
2022-09-21 17:10:42 +0800

    feat: 新增《深入浅出Node.js》随笔

---
+++ books/node/chapter1/server.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var querystring = require('querystring');
+// 侦听服务器的request事件 
+http.createServer(function (req, res) {
+    var postData = ''; 
+    req.setEncoding('utf8');
+    // 侦听请求的data事件 
+    req.on('data', function (trunk) {
+        postData += trunk; });
+    // 侦听请求的end事件 
+    req.on('end', function () {
+        res.end(postData); 
+    });
+}).listen(8081); 
+console.log('服务器启动完成');
