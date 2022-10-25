2f54ff2e575daeb9c1c440edfda0a63c889df1ad
hardenzheng <hardenzheng@tencent.com>
2022-10-25 20:07:51 +0800

    feat: vue-basic 18～19，开始使用脚手架

---
+++ books/vue/vue_basic/19-单文件组件/School.vue
@@ -0,0 +1,33 @@
+<template>
+    <!-- 组件结构 -->
+    <div class="demo">
+        <h5>school name: {{schoolName}}</h5>
+        <h5>school address: {{address}}</h5>
+        <button @click='showName'>click me to show school name</button>
+    </div>
+</template>
+
+<script>
+    // 组件交互的相关代码
+    export default {
+        name: 'School',
+        data(){
+            return {
+                schoolName: 'yunyang',
+                address:'shiyan',
+            }
+        },
+        methods: {
+            showName(){
+                alert(this.schoolName)
+            }
+        }
+    }
+</script>
+
+<style>
+    /* 组件的样式 */
+    .demo{
+        background-color: bisque;
+    }
+</style>
---
+++ books/vue/vue_basic/19-单文件组件/main.js
@@ -0,0 +1,11 @@
+import App from "./App"
+
+new Vue({
+    el: '#root',
+    template: `
+        <App></App>
+    `,
+    components:{
+        App
+    },
+})
