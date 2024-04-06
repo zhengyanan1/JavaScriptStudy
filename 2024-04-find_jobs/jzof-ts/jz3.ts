453e0766c9ec6a3b03ec41827deaf76967fd5365
zhengyanan <zhengyanan.harden@bytedance.com>
2024-04-06 18:19:49 +0800

    feat: 添加jz3

---
+++ 2024-04-find_jobs/jzof-ts/jz3.ts
@@ -0,0 +1,12 @@
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ * 
+ * @param numbers int整型一维数组 
+ * @return int整型
+ */
+export function duplicate(numbers: number[]): number {
+  // write code here
+
+  
+}
